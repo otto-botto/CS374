043e7fd90a6228dad426697e69e8f34ff1c3e885
Lora Edens <lora.l.edens@gmail.com>
2026-02-19 18:14:29 -0800

    Made an array stack and push/pop Levels

--- prog5.c
+++ prog5.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #define STR_LEN 33
 
+typedef struct{
+    int type; // 0 for long, 1 for double, and 2 for string
+    long int longValue;
+    double doubleValue;
+    char stringValue[33];
+}Level;
+
+void push(Level* arrayStack, Level new, int* top) {
+    if (*top != 4) {
+        (*top)++;
+        arrayStack[*top] = new;
+    }else {
+        arrayStack[*top] = new;
+    }
+}
+
+void pop(int* top) {
+    if ((*top)!= -1) {
+        (*top)--;
+    }
+}
+
 
 int check_input(char* input, int length){
     // check if valid long integer, all numbers and no '.'
@@ -34,9 +56,9 @@ int main() {
     size_t bufferSize = STR_LEN;
     int numCharsEntered = -5;
 
-
     input = calloc(bufferSize, sizeof(char));
-
+    int top = -1;
+    Level myArrayStack[5];
 
     while (1) {
         // prompt
@@ -45,7 +67,6 @@ int main() {
         if(numCharsEntered == -1){
             break;
         }
-
 
         // remove the '\n' from getline()
         //    -- find the last char at input[length - 1] and replace it with \0
@@ -56,21 +77,32 @@ int main() {
         // check if entry is valid
         int result = check_input(input, numCharsEntered);
         switch(result){
-            case 0:
-                printf("Valid long.\n");
+            case 0: {
+                // don't need to capture endptr like in check_input, because validated long, pass NULL
+                Level newLevel = {result, strtol(input, NULL,10), 0.0, ""};
+                push(myArrayStack, newLevel, &top);
                 break;
-            case 1:
-                printf("Valid double.\n");
+            }
+            case 1: {
+                // don't need to capture endptr like in check_input, because validated double, pass NULL
+                Level newLevel = {result, 0, strtod(input, NULL), ""};
+                push(myArrayStack, newLevel, &top);
                 break;
-            case 2:
+            }
+            case 2:{
                 if(numCharsEntered <= STR_LEN){
-                    printf("Valid string.\n");
+                    Level newLevel = {result, 0, 0.0, ""};
+                    strcpy(newLevel.stringValue, input);
+                    push(myArrayStack, newLevel, &top);
                     break;
-                } // longer, go to the default
-            default:
-                printf("Invalid input.\n");
-                break;
+                } // longer, go to the default}
+            default: {
+                    printf("Invalid input.\n");
+                    break;
+                }
         }
     }
+    free(input);
+    return 0;
 }
 
